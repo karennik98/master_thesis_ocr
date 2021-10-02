1f66d9fc80218a830245e94b69d4032c5e13adcb
karennik98 <karen.nikoghosyan.98@gmail.com>
2021-10-03 02:30:12 +0400

    Added output file extension change support with args

--- scripts/split_labels.py
+++ scripts/split_labels.py
@@ -6,7 +6,11 @@ def parse_arguments():
         description="Read labels file and create .txt files for all lines"
     )
     parser.add_argument(
-        "--labels_file", type=str, nargs="?", help="The labels file", default="labels.txt"
+        "--labels_file",
+        type=str,
+        nargs="?",
+        help="The labels file",
+        default="labels.txt"
     )
     parser.add_argument(
         "--extension",
@@ -15,16 +19,23 @@ def parse_arguments():
         help="Define the extension of generated labels file",
         default="jpg",
     )
+    parser.add_argument(
+        "--out_extension",
+        type=str,
+        nargs="?",
+        help="Define the extension of generated files",
+        default="txt",
+    )
     return parser.parse_args()
 
-def main(labels_path, extension):
+def main(labels_path, extension, out_extension):
     file = open(labels_path, "r")
     lines = file.readlines()
     for line in lines:
         pattern = r'.+?.' + extension
         new_file_name = re.search(pattern, line).group()
         if new_file_name:
-            new_file_name = new_file_name.replace(extension, "txt")
+            new_file_name = new_file_name.replace(extension, out_extension)
             new_file = open(new_file_name, "w")
             line = re.sub(r'.+?.' + extension + ' ', '', line)
             new_file.write(line)
@@ -32,4 +43,4 @@ def main(labels_path, extension):
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.labels_file, args.extension)
+    main(args.labels_file, args.extension, args.out_extension)
