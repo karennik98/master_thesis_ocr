c09e9d11706a155456cbfb6e2f19a97707577eeb
karennik98 <karen.nikoghosyan.98@gmail.com>
2021-10-03 02:25:28 +0400

    Added extension support in the spliter script

--- scripts/split_labels.py
+++ scripts/split_labels.py
@@ -8,20 +8,28 @@ def parse_arguments():
     parser.add_argument(
         "--labels_file", type=str, nargs="?", help="The labels file", default="labels.txt"
     )
+    parser.add_argument(
+        "--extension",
+        type=str,
+        nargs="?",
+        help="Define the extension of generated labels file",
+        default="jpg",
+    )
     return parser.parse_args()
 
-def main(labels_path):
+def main(labels_path, extension):
     file = open(labels_path, "r")
     lines = file.readlines()
     for line in lines:
-        new_file_name = re.search(r'.+?.jpg', line).group()
+        pattern = r'.+?.' + extension
+        new_file_name = re.search(pattern, line).group()
         if new_file_name:
-            new_file_name = new_file_name.replace("jpg", "txt")
+            new_file_name = new_file_name.replace(extension, "txt")
             new_file = open(new_file_name, "w")
-            line = re.sub(r'.+?.jpg ', '', line)
+            line = re.sub(r'.+?.' + extension + ' ', '', line)
             new_file.write(line)
             new_file.close()
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.labels_file)
+    main(args.labels_file, args.extension)
