8a763326663d3c5707146f1986c73cb478eea846
karennik98 <karen.nikoghosyan.98@gmail.com>
2021-11-16 20:38:29 +0400

    all

---
+++ scripts/pytess.py
@@ -0,0 +1,10 @@
+import tesserocr
+from PIL import Image
+
+print(tesserocr.tesseract_version())  # print tesseract-ocr version
+print(tesserocr.get_languages())  # prints tessdata path and list of available languages
+
+# image = Image.open('sample.jpg')
+# print(tesserocr.image_to_text(image))  # print ocr text from image
+# or
+# print(tesserocr.file_to_text('sample.jpg'))
